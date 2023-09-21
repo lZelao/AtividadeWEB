1200f58ce9d0b2dd5e6d921badc6b55a95249fc9
Eliseu <esreis123@gmail.com>
2023-09-21 13:04:35 -0300

    replicaDapagina

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+// Configuração da pasta de arquivos estáticos (onde o HTML será servido)
+app.use(express.static('public'));
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Inicia o servidor na porta especificada
+app.listen(3030, () => {
+  console.log(`Servidor rodando em http://localhost:3030`);
+});
